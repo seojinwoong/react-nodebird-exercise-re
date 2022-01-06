7efddc0df5e37a9a92584dcf99ea6d40de3cc320
Jinwoong <tjwlsdnd12@gmail.com>
2022-01-06 18:36:49 +0900

    20210106 연습코드 커밋

---
+++ prepare/front/components/CommentForm.js
@@ -0,0 +1,7 @@
+const CommentForm = () => {
+    return (
+
+    );
+}
+
+export default CommentForm;
---
+++ prepare/front/components/PostImages.js
@@ -0,0 +1,13 @@
+import PropType from 'prop-types';
+
+const PostImages = ({ images }) => {
+    return (
+        <div>구현중...</div>
+    )
+}
+
+PostImages.propTypes = {
+    images: PropType.arrayOf(PropType.object)
+}
+
+export default PostImages;
