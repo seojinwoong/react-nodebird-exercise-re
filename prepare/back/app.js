bdb09d08fd33dfde479024302e2ac5079c5eb022
Jinwoong <tjwlsdnd12@gmail.com>
2022-01-21 18:42:38 +0900

    연습코드 최근까지 커밋

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const postRouter = require('./routes/post');
 
 const app = express();
 
@@ -10,18 +11,7 @@ app.get('/api', (req, res) => {
     res.send('hello api');
 });
 
-app.get('/api/posts', (req, res) => {
-    res.json([
-        {id: 1, content: 'hello'},
-        {id: 2, content: 'hello'},
-        {id: 3, content: 'hello'},
-    ])
-})
-
-app.post('/api/poststst', (req, res) => {
-    res.json({ id: 1, content: 'hello' })
-});
-
+app.use('/post', postRouter);
 
 app.listen(3088, () => {
     console.log('서버실행중');
